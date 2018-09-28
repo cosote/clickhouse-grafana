5c607ede3a88d913e67253fc917e941ff8e013d0
jbornemann <jb@prolion.at>
2018-09-28 08:35:17 +0200

    add template engine for queries based on http://krasimirtsonev.com/blog/article/Javascript-template-engine-in-just-20-line

--- dist/sql_query.js
+++ dist/sql_query.js
@@ -2,6 +2,31 @@
 System.register(['lodash', 'app/core/utils/datemath', 'moment', './scanner'], function(exports_1) {
     var lodash_1, dateMath, moment_1, scanner_1;
     var durationSplitRegexp, SqlQuery;
+	
+	//http://krasimirtsonev.com/blog/article/Javascript-template-engine-in-just-20-line
+	var TemplateEngine = function(html, options) {
+		var re = /<%(.+?)%>/g, 
+			reExp = /(^( )?(var|if|for|else|switch|case|break|{|}|;))(.*)?/g, 
+			code = 'with(obj) { var r=[];\n', 
+			cursor = 0, 
+			result,
+				match;
+		var add = function(line, js) {
+			js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+				(code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+			return add;
+		}
+		while(match = re.exec(html)) {
+			add(html.slice(cursor, match.index))(match[1], true);
+			cursor = match.index + match[0].length;
+		}
+		add(html.substr(cursor, html.length - cursor));
+		code = (code + 'return r.join(""); }').replace(/[\r\t\n]/g, ' ');
+		try { result = new Function('obj', code).apply(options, [options]); }
+		catch(err) { console.error("'" + err.message + "'", " in \n\nCode:\n", code, "\n"); }
+		return result;
+	};
+
     return {
         setters:[
             function (lodash_1_1) {
@@ -99,6 +124,8 @@ System.register(['lodash', 'app/core/utils/datemath', 'moment', './scanner'], fu
                         .replace(/\$interval/g, interval)
                         .replace(/\$adhoc/g, renderedAdHocCondition)
                         .replace(/(?:\r\n|\r|\n)/g, ' ');
+					var rawQuery = TemplateEngine(this.target.rawQuery, {options: options});
+					if (rawQuery) this.target.rawQuery = rawQuery;
                     return this.target.rawQuery;
                 };
                 // $columns(query)
